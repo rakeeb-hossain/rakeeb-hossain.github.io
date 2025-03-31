8c71dced0e01381059587383dd4ce638e9358578
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2025-03-31 19:06:26 -0400

    rewrite in Astro

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [[rehypeKatex, { output: 'mathml' }]]
+  })]
+});
