53be8017af6443f16c3451dd89a17f06979aafce
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2025-03-31 20:26:29 -0400

    fix page titles + formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import mdx from '@astrojs/mdx';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [mdx({
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [[rehypeKatex, { output: 'mathml' }]]
-  })]
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [[rehypeKatex, { output: "mathml" }]],
+    }),
+  ],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 const articles = defineCollection({
-    loader: glob({ pattern: '*.{md,mdx}', base: 'src/articles' }),
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-    }),
+  loader: glob({ pattern: "*.{md,mdx}", base: "src/articles" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+  }),
 });
 
 export const collections = {
   articles,
-}; 
+};
